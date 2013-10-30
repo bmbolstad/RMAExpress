e88ab35dde7c80cac5bf825bbef9c6c473f8e0c8
bmbolstad <bmb@bmbolstad.com>
2013-10-30 07:19:49 -0700

    Preparing version string for first public alpha

--- version_number.h
+++ version_number.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_NUMBER_H
 #define VERSION_NUMBER_H
 
-static const wxString version_number = _T("1.1.0 In Development");
+static const wxString version_number = _T("1.1.0 In Development  (v1.10.0-alpha-3)");
 static const wxString copyright_notice = _T("Copyright (c) 2003-2013 B. M. Bolstad  bmb@bmbolstad.com");
 
 #endif
