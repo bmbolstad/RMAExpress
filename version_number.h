ff6f8ba0d0576c34b52c8e4199e6d61576e88ff3
bmbolstad <bmb@bmbolstad.com>
2014-09-15 07:41:47 -0700

    Fix data convertor pgf/clf/mps version mismatch

--- version_number.h
+++ version_number.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_NUMBER_H
 #define VERSION_NUMBER_H
 
-static const wxString version_number = _T("1.1.0");
+static const wxString version_number = _T("1.1.1");
 static const wxString copyright_notice = _T("Copyright (c) 2003-2014 B. M. Bolstad  bmb@bmbolstad.com");
 
 #endif
