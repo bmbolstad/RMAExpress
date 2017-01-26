d921ffca890c68ece18b099dbed850a9d494602d
bmbolstad <bmb@bmbolstad.com>
2017-01-25 21:10:47 -0800

    Increment version number

--- version_number.h
+++ version_number.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_NUMBER_H
 #define VERSION_NUMBER_H
 
-static const wxString version_number = _T("1.2.0 In Development  (v1.20.0-alpha-3)");
+static const wxString version_number = _T("1.2.0 In Development  (v1.20.0-alpha-4)");
 static const wxString copyright_notice = _T("Copyright (c) 2003-2015 B. M. Bolstad  bmb@bmbolstad.com");
 
 #endif
